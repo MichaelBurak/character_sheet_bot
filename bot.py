c23483b66997edf31767f2011238fb17c40f0a26
MichaelBurak <22567541+MichaelBurak@users.noreply.github.com>
2020-09-17 06:22:36 -0600

    commenting and working towards deploy w proc and req file

--- bot.py
+++ bot.py
@@ -23,9 +23,40 @@ sh = gc.open(SHEET)
 worksheet = sh.get_worksheet(0)
 
 '''BEGIN COMMANDS
------------------------- '''
+------------------------ 
 
-#set spreadsheet to user input 
+Resources and methods to map out and code: 
+-Spreadsheets
+    -Reading?
+    -Updating current spreadsheet
+-Tabs(worksheets)
+    -Create
+    -Reading?
+    -Update current 
+-Cells
+    -Create
+    -Reading (values)
+    -Updating (values)
+    -Deleting (clearing)
+        -Columns
+            -Create
+            -Reading
+            -Updating
+            -Deleting
+        -Rows
+            -Create
+            -Reading
+            -Updating
+            -Deleting
+        -Ranges 
+            -Create
+            -Reading
+            -Updating
+            -Deleting
+'''
+
+#---Sheets---
+#UPDATE current spreadsheet from user input
 @bot.command()
 async def setsheet(ctx,sheet):
     global sh
@@ -35,14 +66,18 @@ async def setsheet(ctx,sheet):
     response = f"Current sheet: {sh.title}"
     await ctx.send(response)
 
-#add new tab to current google sheet with base 100 row and column
+#---Tabs---
+#CREATE new tab to current google sheet with base 100 row and column
 @bot.command()
 async def addtab(ctx,title="fill this in",rows=100,cols=100):
     sh.add_worksheet(title,rows,cols)
     response = f"Tab of name --{title}-- with {rows} rows and {cols} added to spreadsheet {sh.title}"
     await ctx.send(response)
 
-#switch to user provided tab by name
+#READ
+#Reading whole tab to pandas or other libraries?
+
+#UPDATE current tab to user provided tab by name
 @bot.command()
 async def switchtab(ctx,tab):
     global worksheet
@@ -50,21 +85,34 @@ async def switchtab(ctx,tab):
     response = f"Switched tab of spreadsheet to {tab}"
     await ctx.send(response)
 
-#U-pdate a given cell's value
+#DELETE to clear whole tab
+#Wipe a sheet clear with option to duplicate to a hidden sheet a backup?
+
+#---Cells---
+
+#CREATE a cell?
+#Does this have a use case on a properly formatted sheet? This is probably create row/column coverage.
+
+#CREATE a column or row at given position
+@bot.command()
+async def createcells(ctx,direction,n,idx):
+    #
+
+#UPDATE a given cell's value
 @bot.command()
 async def editcell(ctx,cell,val):
     worksheet.update(cell, val)
     response = f"Cell at position {cell} of worksheet --{worksheet.title}-- changed value to {val}"
     await ctx.send(response)
 
-#R-ead a given cell's value
+#READ a given cell's value
 @bot.command()
 async def readcell(ctx,cell):
     read_val = worksheet.acell(cell).value
     response = f"Cell at position {cell} of worksheet --{worksheet.title}-- value is {read_val}"
     await ctx.send(response)
 
-#R-ead a given column or row w/cells delimeted by space
+#READ a given column or row w/cells delimeted by space
 @bot.command()
 async def readcells(ctx, direction, idx):
     val_list = []
