9fd9a586976c24076bcf045370c098c8216cb255
MichaelBurak <22567541+MichaelBurak@users.noreply.github.com>
2020-09-16 21:02:41 -0600

    adding reading rows and cols

--- bot.py
+++ bot.py
@@ -12,10 +12,13 @@ gc = gspread.oauth()
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+SHEET = os.getenv("BASE_SHEET")
 
 bot = commands.Bot(command_prefix='!')
 
-worksheet = None 
+#base sheet setting, hide/comment out on commits
+sh = gc.open(SHEET)
+worksheet = sh.get_worksheet(0)
 
 @bot.command()
 async def setsheet(ctx,sheet):
@@ -51,6 +54,20 @@ async def readcell(ctx,cell):
     read_val = worksheet.acell(cell).value
     response = f"Cell at position {cell} of worksheet --{worksheet.title}-- value is {read_val}"
     await ctx.send(response)
+
+@bot.command()
+async def readcells(ctx, direction, idx):
+    val_list = []
+    if direction == "row":
+        val_list = worksheet.row_values(idx)
+        response = f"Row no. {idx} reads {' '.join(val_list)}"
+        await ctx.send(response)
+    elif direction == "column":
+        val_list = worksheet.col_values(idx)
+        response = f"Column no. {idx} reads {' '.join(val_list)}"
+        await ctx.send(response)
+    else:
+        await ctx.send("Please select row or column as the first argument")
 
 
 bot.run(TOKEN)
