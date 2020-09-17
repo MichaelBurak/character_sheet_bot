8674a97fe8cfd79c9f99ebf17691f4592f9b637b
MichaelBurak <22567541+MichaelBurak@users.noreply.github.com>
2020-09-16 21:13:58 -0600

    mostly comments for idiomatics

--- bot.py
+++ bot.py
@@ -7,19 +7,25 @@ import gspread
 from discord.ext import commands
 from dotenv import load_dotenv
 
+#oAuth gspread to access google drive/sheets
 gc = gspread.oauth()
 
-
+#env var handling of token and testing Google Sheet
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 SHEET = os.getenv("BASE_SHEET")
 
+#prefix for cmnds is !
 bot = commands.Bot(command_prefix='!')
 
-#base sheet setting, hide/comment out on commits
+#base sheet setting to env SHEET's first tab
 sh = gc.open(SHEET)
 worksheet = sh.get_worksheet(0)
 
+'''BEGIN COMMANDS
+------------------------ '''
+
+#set spreadsheet to user input 
 @bot.command()
 async def setsheet(ctx,sheet):
     global sh
@@ -29,12 +35,14 @@ async def setsheet(ctx,sheet):
     response = f"Current sheet: {sh.title}"
     await ctx.send(response)
 
+#add new tab to current google sheet with base 100 row and column
 @bot.command()
 async def addtab(ctx,title="fill this in",rows=100,cols=100):
     sh.add_worksheet(title,rows,cols)
     response = f"Tab of name --{title}-- with {rows} rows and {cols} added to spreadsheet {sh.title}"
     await ctx.send(response)
 
+#switch to user provided tab by name
 @bot.command()
 async def switchtab(ctx,tab):
     global worksheet
@@ -42,19 +50,21 @@ async def switchtab(ctx,tab):
     response = f"Switched tab of spreadsheet to {tab}"
     await ctx.send(response)
 
-
+#U-pdate a given cell's value
 @bot.command()
 async def editcell(ctx,cell,val):
     worksheet.update(cell, val)
     response = f"Cell at position {cell} of worksheet --{worksheet.title}-- changed value to {val}"
     await ctx.send(response)
 
+#R-ead a given cell's value
 @bot.command()
 async def readcell(ctx,cell):
     read_val = worksheet.acell(cell).value
     response = f"Cell at position {cell} of worksheet --{worksheet.title}-- value is {read_val}"
     await ctx.send(response)
 
+#R-ead a given column or row w/cells delimeted by space
 @bot.command()
 async def readcells(ctx, direction, idx):
     val_list = []
@@ -69,5 +79,8 @@ async def readcells(ctx, direction, idx):
     else:
         await ctx.send("Please select row or column as the first argument")
 
+'''END COMMANDS
+-------------------- '''
 
+#run the bot using env TOKEN
 bot.run(TOKEN)
